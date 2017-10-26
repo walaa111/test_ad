478c2b2aba2e19347083a0f1c0d4092625a75fcf
walaa111 <walaagamil18@gmail.com>
2017-10-26 14:38:06 +0200

    Update admob.js

--- test_ad/www/js/admob.js
+++ test_ad/www/js/admob.js
@@ -1,26 +1,32 @@
-alert("yes");
 var admobid={}
   if( /(android)/i.test(navigator.userAgent) ) {
   admobid = { // for Android
-  banner: 'ca-app-pub-xxxxx/xxxxx',
-  interstitial: 'ca-app-pub-xxxxxx/xxxxxx'
+  banner: 'ca-app-pub-8356095399620813~1240630064',
+  interstitial: 'ca-app-pub-8356095399620813/1521553863'
   }
 }
 else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
   admobid = { // for iOS
-  banner: 'ca-app-pub-xxxxxxx/xxxxxxx',
-  interstitial: 'ca-app-pub-xxxxxxx/xxxxxxxx'
+  banner: 'ca-app-pub-8356095399620813~1240630064',
+  interstitial: 'ca-app-pub-8356095399620813/1521553863'
   };
 }
 else {
   admobid = { // for Windows Phone
-  banner: 'ca-app-pub-xxxxxxx/xxxxxxx',
-  interstitial: 'ca-app-pub-xxxxxxx/xxxxxxxx'
+  banner: 'ca-app-pub-8356095399620813~1240630064',
+  interstitial: 'ca-app-pub-8356095399620813/1521553863'
   };
 }
-//code for pop-up-ads
-if(AdMob) AdMob createBanner({
-  adId:admobid.banner,
-  position:AdMob.AD_POSITION.BOTTOM_CENTER,
-  autoShow:true
+
+AdMob.createBanner( admobid.banner );
+
+AdMob.createBanner({
+	adId: admobid.banner,
+	position: AdMob.AD_POSITION.BOTTOM_CENTER,
+	autoShow: true,
+	success: function(){
+	},
+	error: function(){
+		alert('failed to create banner');
+	}
 });
